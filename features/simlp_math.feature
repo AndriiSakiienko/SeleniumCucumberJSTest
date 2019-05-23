b03a42f7c49f7104f47eb554d185dbcac936e4dd
Andrii Sakiienko <andrii.sakiienko@gmail.com>
2019-05-23 13:53:00 +0300

    chng example
    take another example and run the script

--- features/simlp_math.feature
+++ features/simlp_math.feature
@@ -1,19 +1,13 @@
-Feature: Simple maths
-  In order to do maths
-  As a developer
-  I want to increment variables
+Feature: Is it Friday yet?
+    Everybody wants to know when it's Friday
 
-  Scenario: easy maths
-    Given a variable set to 1
-    When I increment the variable by 1
-    Then the variable should contain 2
+Scenario Outline: Today is or is not Friday
+    Given today is "<day>"
+    When I ask whether it's Friday yet
+    Then I should be told "<answer>"
 
-  Scenario Outline: much more complex stuff
-    Given a variable set to <var>
-    When I increment the variable by <increment>
-    Then the variable should contain <result>
-
-    Examples:
-      | var | increment | result |
-      | 100 |         5 |    105 |
-      |  99 |      1234 |   1333 |
+Examples:
+    | day            | answer |
+    | Friday         | TGIF   |
+    | Sunday         | Nope   |
+    | anything else! | Nope   |
--- features/step_definitions/step_definitions.js
+++ features/step_definitions/step_definitions.js
@@ -1,55 +1,22 @@
- // Cucumber and chai have been loaded in the browser
-const Cucumber = require('cucumber');
-var setWorldConstructor = Cucumber.setWorldConstructor;
-var Given = Cucumber.Given;
-var When = Cucumber.When;
-var Then = Cucumber.Then;
-//var expect = chai.expect; //TODO: what is that? investigate
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
 
-///// World /////
-//
-// Call 'setWorldConstructor' with to your custom world (optional)
-//
+function isItFriday(today) {
+    if (today === "Friday") {
+        return "TGIF";
+    } else {
+        return "Nope";
+    }
+}
 
-var CustomWorld = function() {
-  this.variable = 0;
-};
-
-CustomWorld.prototype.setTo = function(number) {
-  this.variable = parseInt(number);
-};
-
-CustomWorld.prototype.incrementBy = function(number) {
-  this.variable += parseInt(number);
-};
-
-setWorldConstructor(CustomWorld);
-/**/
-///// Step definitions /////
-//
-// use 'Given', 'When' and 'Then' to declare step definitions
-//
-Given('I am on the Cucumber.js GitHub repository', function () {
-    return this.driver.get('https://github.com/cucumber/cucumber-js/tree/master')
+Given('today is {string}', function (givenDay) {
+    this.today = givenDay;
 });
 
-Given('I closed the Join GitHub today Dialogue', function () {
-    var locator = "//button[@type='submit']";
-    return this.driver.findElement({xpath: locator}).then(function (element) {
-        return element.click();
-    });
+When('I ask whether it\'s Friday yet', function () {
+    this.actualAnswer = isItFriday(this.today);
 });
 
-
-When('I click on {stringInDoubleQuotes}', function (text) {
-    return this.driver.findElement({linkText: text}).then(function (element) {
-        return element.click();
-    });
-});
-
-Then('I should see {stringInDoubleQuotes}', function (text) {
-    var xpath = "//*[contains(text(),'" + text + "')]";
-    //var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
-    //return this.driver.wait(condition, 5000);
-    return this.waitForElement(xpath);
+Then('I should be told {string}', function (expectedAnswer) {
+    assert.equal(this.actualAnswer, expectedAnswer);
 });
